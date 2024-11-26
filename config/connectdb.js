107f30be6e62fed7a3906468f9db3c6a45c060cc
Yosr belaid <belaidyosr4@gmail.com>
2024-11-26 16:31:20 +0100

    first commit

--- config/connectdb.js
+++ config/connectdb.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 const connectdb = () =>{
     mongoose
-    .connect('mongodb+srv://yosrbelaid:yosr1234@cluster1.iqorj.mongodb.net/contact?retryWrites=true&w=majority&appName=Cluster1')
+    .connect('mongodb+srv://yosrbelaid:******@cluster1.iqorj.mongodb.net/contact?retryWrites=true&w=majority&appName=Cluster1')
     .then((console.log('mongoose connected')))
     .catch((err)=>console.log(err))
 }
